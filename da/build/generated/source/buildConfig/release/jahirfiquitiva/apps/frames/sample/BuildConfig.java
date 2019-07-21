f1a625b4c2c318efb9273f9979cb2aeb1640fa23
rajkale99 <kaleraj.rk@gmail.com>
2019-07-21 08:39:30 +0000

    Travis build 3 pushed

---
+++ da/build/generated/source/buildConfig/release/jahirfiquitiva/apps/frames/sample/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jahirfiquitiva.apps.frames.sample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "jahirfiquitiva.apps.frames.sample";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 262;
+  public static final String VERSION_NAME = "2.6.2";
+}
