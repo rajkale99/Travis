8304c0b64b3113fa737ba50219473f2bcea48811
rajkale99 <kaleraj.rk@gmail.com>
2019-07-27 18:55:11 +0000

    Travis build 57 pushed

--- qw/build/generated/source/buildConfig/release/com/legion/lpaper/app/BuildConfig.java
+++ qw/build/generated/source/buildConfig/release/com/legion/lpaper/app/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.legion.lpaper";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 262;
-  public static final String VERSION_NAME = "2.6.2";
+  public static final int VERSION_CODE = 100;
+  public static final String VERSION_NAME = "1.0.0";
 }
